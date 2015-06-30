e8b64c15b83d12d3e1dea98b22a7c8559f276d96
Hang Yuan <yuanhang3260@gmail.com>
2015-06-30 16:15:02 -0700

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -32,6 +32,27 @@ int main(int argc, char** argv) {
   //   fprintf(stderr, "set socket option IP_HDRINCL failed\n");
   //   return -1;
   // }
+  
+  int socket_priority = 0;
+  if (setsockopt(send_fd, SOL_SOCKET, SO_PRIORITY,
+                 &socket_priority, sizeof(socket_priority)) < 0) {
+    fprintf(stderr, "set socket option SO_PRIORITY failed\n");
+    return -1;
+  }
+
+  uint32_t x = 1;
+  if (setsockopt(send_fd, IPPROTO_IPV6, IPV6_MULTICAST_LOOP,
+                 &x, sizeof(x)) < 0) {
+    fprintf(stderr, "set socket option IPV6_MULTICAST_LOOP failed\n");
+    return -1;
+  }
+
+  int is_on = 1;
+  if (setsockopt(send_fd, SOL_SOCKET, SO_BROADCAST,
+                 &is_on, sizeof(is_on)) < 0) {
+    fprintf(stderr, "set socket option SO_BROADCAST failed\n");
+    return -1;
+  }
 
   char buf[32];
   strncpy(buf, "hello\0", 6);
